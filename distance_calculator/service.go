c701583d81617e368f65d0d6baa2eeb124013f2a
mhg14 <mohamadhasanghamari@yahoo.com>
2023-06-22 18:31:06 +0330

    refactoring the calculator service to decrease memory consumption

--- distance_calculator/service.go
+++ distance_calculator/service.go
@@ -12,23 +12,20 @@ type CalculatorServicer interface {
 }
 
 type CalculatorService struct {
-	points [][]float64
+	prevPoint []float64
 }
 
 func NewCalculatorService() *CalculatorService {
-	return &CalculatorService{
-		points: make([][]float64, 0),
-	}
+	return &CalculatorService{}
 }
 
 func (s *CalculatorService) CalculateDistance(data types.OBUData) (float64, error) {
-	fmt.Println("calculating the distance")
 	var distance float64 = 0.0
-	if len(s.points) > 0 {
-		prevPoint := s.points[len(s.points)-1]
-		distance = calculateDistance(prevPoint[0], prevPoint[1], data.Lat, data.Long)
+
+	if len(s.prevPoint) > 0 {
+		distance = calculateDistance(s.prevPoint[0], s.prevPoint[1], data.Lat, data.Long)
 	}
-	s.points = append(s.points, []float64{data.Lat, data.Long})
+	s.prevPoint = []float64{data.Lat, data.Long}
 	return distance, nil
 }
 
